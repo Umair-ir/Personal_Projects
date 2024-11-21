dbc6c4431a275ac2312d0e9cb937726ac322fb3f
Umair-ir <153264755+Umair-ir@users.noreply.github.com>
2024-11-21 03:17:06 -0500

    Update ASM_SqWave.py

--- ASM_SqWave.py
+++ ASM_SqWave.py
@@ -7,8 +7,10 @@ class sqWave:
     def __init__(self,frequency):
 
         self.frequency=frequency
-        '''A transfer function or lookup table will be required to convert the set 
-        frequency into a value that can be written to the ASM delay variable.'''
+        
+        # Convert frequency to delay and then to hex
+        delay_value = hex(int((1 / frequency) * 1000000)) 
+
 
         with open('[path to file]') as file:
             lines=[line.rstrip() for line in file.readlines()]
@@ -27,9 +29,12 @@ class sqWave:
         os.chdir() #to navigate to the directory
         process=subprocess.Popen('make',shell=False,stdout=subprocess.PIPE, stderr=subprocess.DEVNULL)
         process.wait()
-        self.pi=subprocess.Popen('./[name of .s file]' = False, stdout=subprocess.PIPE,stderr=subprocess.DEVNULL)
+        self.p1 = subprocess.Popen('./BlinkLed', shell=False, stdout=subprocess.PIPE, stderr=subprocess.DEVNULL)
 
     def stop(self):
         self.p1.terminate()
 
-    def setFrequency(self,frequency):
+    def setFrequency(self, frequency):
+        # Update the frequency and restart the square wave
+        self.__init__(frequency)
+        self.start()
